f55fbf8ae9b025e875048d66a7ae14dab8314ba9
pompos02 <yiannis.caravellas@gmail.com>
2024-11-19 11:17:42 +0200

    meandros

---
+++ db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+
+
+def get_db():
+    db_name = 'speeches'
+    db_user = 'pompos02'  # Replace with your PostgreSQL username
+    db_password = 'mypassword123'  # Replace with your PostgreSQL password
+    db_host = 'localhost'
+    db_port = '5432'
+    engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')
+    return db_name, db_user, db_password, db_host, db_port, engine
